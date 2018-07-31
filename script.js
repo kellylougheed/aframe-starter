f56ea774135ebbd14ebded9753bfee5c3b54b227
Kelly Lougheed <kellylougheed@gmail.com>
2018-07-31 15:38:47 +0000

    Take sphere out of container so click event listener works

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("A-Frame rocks!")
+let container = document.querySelector("#container")
+let sphere = document.querySelector("#sphere")
+let y = 1.5
+
+sphere.addEventListener('click', addbox)
+
+function addbox() {
+  y += 1
+  container.innerHTML += `<a-box color="dodgerblue" depth="0.5" height="0.5" width="0.5" position='0 ${y} -5'></a-box>`
+}
